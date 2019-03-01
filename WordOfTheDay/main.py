f859fb6360694720a843c98e7e98eb55e4ff29e5
Lame Lemon <garybsantos@gmail.com>
2019-03-01 16:24:58 -0500

    📜 Ratelimiting + bug fixes

--- WordOfTheDay/main.py
+++ WordOfTheDay/main.py
@@ -1,29 +1,38 @@
 '''
 Posts a selftext on the targeted subreddit (SUB) with the last word 
-in words.txt every day at a set time (TIME).
+in words.txt every day at the set time (TIME). Place each new word
+on a new line.
 '''
 
 import praw
 import random
 import datetime
 import schedule
-import time
+from time import sleep, strftime
+from datetime import datetime
 
 reddit = praw.Reddit(username = '', password = '',
                     client_id = '', client_secret= '',
                     user_agent= 'Post Word of the day /u/PeskyPotato')
 
-TIME = '12:44' # Replace with time to run daily
+# Time to run daily
+TIME = '12:44'
 SUB = 'calligraffiti' 
 
 def submitWord():
     word = getWord()
     if word:
         title = "WotD " + datetime.datetime.today().strftime('%d/%m/%y') + " - " + word
-        reddit.subreddit(SUB).submit(title, selftext='Post pictures of your work here!')
-        print("Posted WoTD", word)
+        try: 
+            reddit.subreddit(SUB).submit(title, selftext='Post pictures of your work here!')
+        except praw.exceptions.APIException:
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "Rate limited, sleeping for 15 minutes")
+            sleep(900)
+            reddit.subreddit(SUB).submit(title, selftext='Post pictures of your work here!')
+
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "Posted WoTD", word)
     else:
-        print("There are no words left")
+        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "There are no words left")
 
 def getWord():
     with open('words.txt', 'r') as f:
@@ -36,8 +45,8 @@ def getWord():
             for word in words[:-1]:
                 f.write(word + '\n')
         
-        if len(words) == 1:
-            print("Last word has been used, make sure to add more for tomorrow")
+        if len(words) == 0:
+            print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "Last word has been used, make sure to add more for tomorrow")
         return wordOTD
 
 
@@ -45,5 +54,6 @@ schedule.every().day.at(TIME).do(submitWord)
 
 while True:
     schedule.run_pending()
+    sleep(1)
 
 
