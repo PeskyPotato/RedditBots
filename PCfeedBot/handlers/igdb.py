a7c8784cbbbb2a70554b82689971e95a2373671e
Lame Lemon <garybsantos@gmail.com>
2019-06-09 22:28:28 +0300

    🖥️  database to avoid duplicates

--- PCfeedBot/handlers/igdb.py
+++ PCfeedBot/handlers/igdb.py
@@ -6,11 +6,15 @@ with open('config.json', 'r') as f:
     config_data = json.load(f)
 
 def get_igdb_url(title):
+    title = title
     url = "https://api-v3.igdb.com/games/"
     headers = {"user-key":config_data["igdb_user_key"]}
     body = 'search "{}"; fields url, name;'.format(title)
-
-    r = requests.post(url, data=body, headers=headers)
+    try:
+        r = requests.post(url, data=body, headers=headers)
+    except UnicodeEncodeError:
+        # find a solution for this https://www.reddit.com/r/PCfeed/comments/bxwaqo/octopath_traveler/
+        return None
     if r.status_code == 200:
         data = r.json()
         for game in data:
